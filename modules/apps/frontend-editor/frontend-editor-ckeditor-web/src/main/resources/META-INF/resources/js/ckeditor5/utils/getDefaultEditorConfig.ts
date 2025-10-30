e70b2c5685338d8313d1338b3e9ba2fe50ee8747
Davyson Melo <davyson.melo@liferay.com>
2025-10-30 10:44:33 -0300

    LPD-69639 Add AIDropdownActions to Ckeditor5

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/js/ckeditor5/utils/getDefaultEditorConfig.ts
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/js/ckeditor5/utils/getDefaultEditorConfig.ts
@@ -150,7 +150,7 @@ const getDefaultEditorConfig = ({
 		TableCaption,
 		TableProperties,
 		TableToolbar,
-		AIDropdownActions
+		AIDropdownActions,
 	];
 
 	if (editorVariant === EEditorVariant.CLASSIC) {
