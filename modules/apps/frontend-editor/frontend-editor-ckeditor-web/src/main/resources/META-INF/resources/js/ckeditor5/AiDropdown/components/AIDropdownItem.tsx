b6d00f17ab3473a17048a0a99464ac3b11f3a84b
Davyson Melo <davyson.melo@liferay.com>
2025-10-31 15:31:40 -0300

    LPD-69639 Add logic to disable action and loading state

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/js/ckeditor5/AiDropdown/components/AIDropdownItem.tsx
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/js/ckeditor5/AiDropdown/components/AIDropdownItem.tsx
@@ -3,22 +3,40 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
  
-import ClayIcon, { ClayIconSpriteContext } from '@clayui/icon'
+import ClayIcon from '@clayui/icon';
+import ClayLoaddingIndicator from '@clayui/loading-indicator';
+import React, { useState } from "react";
 
-import React from "react";
+interface action {
+	icon: string;
+	label: string;
+	setNewContent: (newText: string) => void;
+}
 
-
-const AIDropdownItem = ({ item: { label }, setNewContent }: { item: { label: string }, setNewContent: (newText: string) => void },) => {
+const AIDropdownItem = ({icon, label, setNewContent}: action) => {
+	
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [actionDisabled, setActionDisabled] = useState<boolean>(false); 
 
 	function handleClick() {
-		setNewContent("text changed");
+		setIsLoading(true);
+		setActionDisabled(true);
+		setTimeout(() => {
+			setNewContent("text changed");
+			setIsLoading(false);
+			setActionDisabled(false);
+		}, 2000)
 	}
 
 	return (
 		<div className="ai-dropdown-item" onClick={handleClick}>
-			<ClayIcon height={10} spritemap={Liferay.Icons.spritemap} symbol='magic' width={10} />
+			<div>
+				<ClayIcon height={16} opacity={actionDisabled ? 0.4 : 1} spritemap={Liferay.Icons.spritemap} symbol={icon} width={16} />
 
-			<span className="ai-dropdown-item-label">{label}</span>
+				<span className={`ai-dropdown-item-label ${actionDisabled && 'ai-dropdown-item-label-disabled'}`}>{label}</span>
+			</div>
+
+			{isLoading && <ClayLoaddingIndicator className='ai-dropdown-item-loading-indicator' />}
 		</div>
 	);
 };
