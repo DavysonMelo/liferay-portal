7fad4e1ac4bb5e43192ec67cb07f198707b11611
Davyson Melo <davyson.melo@liferay.com>
2025-11-12 11:29:37 -0300

    LPD-71144 Add prompt section

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/Sidebar.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/Sidebar.js
@@ -29,10 +29,6 @@ export const contents = {
 		showDeleteButton: true,
 		title: Liferay.Language.get('actions'),
 	},
-	'llm': {
-		sections: ['nodeInformation'],
-		title: Liferay.Language.get('llm-node'),
-	},
 	'assignments': {
 		backButton: (setContentName) => () => setContentName('task'),
 		deleteFunction: (setSelectedItem) => () =>
