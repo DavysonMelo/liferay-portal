123f4d07dca560bfcccfff71bccc9d3dd5a09306
Davyson Melo <davyson.melo@liferay.com>
2026-05-29 08:01:12 -0300

    LPD-92473 Configure FDS for Issue Reports

--- modules/dxp/apps/ai-hub/ai-hub-web/src/main/java/com/liferay/ai/hub/web/internal/constants/AIHubFDSNames.java
+++ modules/dxp/apps/ai-hub/ai-hub-web/src/main/java/com/liferay/ai/hub/web/internal/constants/AIHubFDSNames.java
@@ -22,6 +22,9 @@ public class AIHubFDSNames {
 	public static final String INSTRUCTION_DEFINITIONS =
 		AIHubWebConstants.BUNDLE_SYMBOLIC_NAME + "-instructionDefinitions";
 
+	public static final String ISSUE_REPORTS =
+		AIHubWebConstants.BUNDLE_SYMBOLIC_NAME + "-issueReports";
+
 	public static final String MODEL_ARMOR_TEMPLATES =
 		AIHubWebConstants.BUNDLE_SYMBOLIC_NAME + "-modelArmorTemplates";
 
